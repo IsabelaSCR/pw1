d86f6701da21e3a96a838c37a56014017cf45bc6
Isabela Sophia C. da Rocha <104832821+IsabelaSCR@users.noreply.github.com>
2023-04-17 00:09:48 -0300

    Add files via upload

---
+++ projeto09/js/externo.js
@@ -0,0 +1,7 @@
+// mensagem ao usuário
+	alert ("É bom ter você aqui, é muito bom!");
+	
+	// criando variáveis
+	var valor01 = 0;
+	var valor02;
+	document.write("O valor default da variável valor01 era "+valor01+" .<br>");
